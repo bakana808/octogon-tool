c8d911edceb91d3616fa74a518c26014fc461d05
Branden Akana <branden.akana@gmail.com>
2020-09-13 05:05:28 -1000

    start smash.gg GUI options

--- main.py
+++ main.py
@@ -3,12 +3,59 @@ from server import start_server, create_server
 import threading
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QKeyEvent
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, qApp
+from PyQt5.QtWidgets import (QLabel, QGridLayout, QLineEdit, QWidget,
+                             QSizePolicy, QSpacerItem, QVBoxLayout)
 
 
-class OctogonWidget(QWidget):
+class OctogonWidget(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        # layout
+
+        wid = QWidget(self)
+        self.setCentralWidget(wid)
+
+        smash_title_label = QLabel("Smash.gg Settings")
+        smash_title_label.setAlignment(Qt.AlignBottom)
+        # smash_title_label.setStyleSheet("QLabel {background-color: red;}")
+        smash_tourny_label = QLabel("Tournament Slug")
+        smash_event_label = QLabel("Event ID")
+
+        smash_tourny_edit = QLineEdit()
+        smash_event_edit = QLineEdit()
+
+        grid = QGridLayout()
+        grid.setSpacing(10)
+
+        # sub-layout for smash.gg related options
+        smash_group = QGridLayout(self)
+        smash_group.addWidget(smash_title_label, 0, 0)
+        smash_group.addWidget(smash_tourny_label, 1, 0)
+        smash_group.addWidget(smash_tourny_edit, 1, 1)
+        smash_group.addWidget(smash_event_label, 2, 0)
+        smash_group.addWidget(smash_event_edit, 2, 1)
+
+        grid.addLayout(smash_group, 0, 0)
+        spacer = QSpacerItem(QSizePolicy.Minimum, QSizePolicy.Expanding)
+        grid.addItem(spacer, 1, 0, 1, -1)
+
+        wid.setLayout(grid)
+
+        # menubar
+
+        action = QAction("&Exit", self)
+        action.setShortcut('Ctrl+C')
+        action.setStatusTip('Close the panel')
+        action.triggered.connect(qApp.quit)
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(action)
+
+        self.statusBar().showMessage('Ready')
+
         self.setGeometry(300, 300, 800, 600)
         self.setWindowTitle("Octogon Panel")
         self.show()
@@ -33,8 +80,8 @@ def main():
         thread.start()
 
         app.exec()
-    except KeyboardInterrupt:
-        pass
+    except Exception as e:
+        print(e)
     finally:
         server.shutdown()
         print("server has stopped.")
