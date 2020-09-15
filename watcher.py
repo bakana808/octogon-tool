34f29088f926f16ba815a1bac4e1df382993c514
Branden Akana <branden.akana@gmail.com>
2020-09-15 03:13:25 -1000

    enable path searching for scss imports

--- watcher.py
+++ watcher.py
@@ -15,29 +15,36 @@ def scss_compile(path):
         output_path = os.path.splitext(path)[0] + ".css"
         print(f"compiling SCSS at {path}")
         with open(output_path, "w") as f:
-            f.write(Compiler().compile(path))
-    except SyntaxError as e:
-        print(f"failed to compile scss: ")
+            f.write(Compiler(search_path=("style/",)).compile(path))
+    except Exception as e:
+        print("failed to compile scss: ")
         print(e)
 
 
 def scss_batch_compile(path):
     """Compiles all SCSS files in a folder."""
 
-    files = [
-        f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))
-    ]
-    files = [os.path.join(path, f) for f in files]
+    print(f"compiling all SCSS files in {path}")
 
-    for filepath in files:
-        scss_compile(filepath)
+    try:
+        files = [
+            f
+            for f in os.listdir(path)
+            if os.path.isfile(os.path.join(path, f))
+        ]
+        files = [os.path.join(path, f) for f in files]
+
+        for filepath in files:
+            scss_compile(filepath)
+    except Exception as e:
+        print(e)
 
 
 class SCSSParserEventHandler(FileSystemEventHandler):
     def on_modified(self, event: FileModifiedEvent):
         path = event.src_path
-
-        scss_compile(path)
+        if os.path.splitext(path)[1] == ".scss":
+            scss_batch_compile(os.path.dirname(path))
 
 
 def start_watcher():
