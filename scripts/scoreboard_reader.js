7a3b39ad24d38c6b76e43cf544af873cea1b7b99
Branden Akana <branden.akana@gmail.com>
2020-09-17 01:45:23 -1000

    implement win counter updater and player name colors

--- scripts/scoreboard_reader.js
+++ scripts/scoreboard_reader.js
@@ -91,25 +91,108 @@ async function fetch_sb_data() {
 }
 
 /**
+ * Updates the win count for the given player.
+ *
+ * 0: Player 1
+ * 1: Player 2
+ */
+async function sb_update_wins(player, wins) {
+	var id;
+	if (player === 0) {
+		id = "p1_wincounter";
+	} else {
+		id = "p2_wincounter";
+	}
+
+	var elm = get_elm(id);
+	var children = elm.childNodes;
+
+	// reverse order of filled icons for P1 side
+	// TODO: change into setting
+	if (player === 0) {
+		children = Array.from(children).reverse();
+	}
+
+	children.forEach((child) => {
+		if (wins > 0) child.classList.add("sb_winicon_filled");
+		else child.classList.remove("sb_winicon_filled");
+		wins--;
+	});
+}
+
+/**
  * Updates the scoreboard.
  */
 async function sb_update() {
 	console.log("fetching data...");
 	data = await fetch_sb_data();
 
+	// WIN COUNT
+
+	data.on_modified("round_games", (games) => {
+		var num_icons = 2;
+		if (games == 5) {
+			num_icons = 3;
+		}
+
+		var p1_wincounter = get_elm("p1_wincounter");
+		// clear all child nodes
+		while (p1_wincounter.firstChild) {
+			p1_wincounter.removeChild(p1_wincounter.lastChild);
+		}
+
+		for (var i = 0; i < num_icons; i++) {
+			var win_elm = document.createElement("div");
+			win_elm.id = "p1_w" + i;
+			win_elm.className = "sb_winicon";
+			win_elm.appendChild(document.createElement("span"));
+			p1_wincounter.appendChild(win_elm);
+		}
+
+		var p2_wincounter = get_elm("p2_wincounter");
+		while (p2_wincounter.firstChild) {
+			p2_wincounter.removeChild(p2_wincounter.lastChild);
+		}
+
+		for (var i = 0; i < num_icons; i++) {
+			var win_elm = document.createElement("div");
+			win_elm.id = "p2_w" + i;
+			win_elm.className = "sb_winicon";
+			win_elm.appendChild(document.createElement("span"));
+			p2_wincounter.appendChild(win_elm);
+		}
+
+		sb_update_wins(0, data.json["p1"]["wins"]);
+		sb_update_wins(0, data.json["p2"]["wins"]);
+	});
+
+	// WIN COUNTER
+
+	data.on_modified("p1.wins", (wins) => {
+		sb_update_wins(0, wins);
+	});
+
+	data.on_modified("p2.wins", (wins) => {
+		sb_update_wins(1, wins);
+	});
+
+	// PLAYER NAME
+
 	data.on_modified("p1.name", (name) => {
 		console.log("updating p1 name");
 		get_elm("p1_name").textContent = name;
 	});
 
-	data.on_modified("p1.character", (character) => {
-		var path = get_portrait_path(character);
-		get_elm("p1_portrait").style.backgroundImage = `url("${path}")`;
-	});
-
 	data.on_modified("p2.name", (name) => {
 		console.log("updating p2 name");
 		get_elm("p2_name").textContent = name;
+	});
+
+	// PLAYER CHARACTER PORTRAIT
+
+	data.on_modified("p1.character", (character) => {
+		var path = get_portrait_path(character);
+		get_elm("p1_portrait").style.backgroundImage = `url("${path}")`;
 	});
 
 	data.on_modified("p2.character", (character) => {
