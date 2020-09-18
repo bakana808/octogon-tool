6a32df7ded63eaed45a305a81bbaa633782f4a42
Branden Akana <branden.akana@gmail.com>
2020-09-17 15:31:44 -1000

    finish basic scoreboard options

---
+++ gui.py
@@ -0,0 +1,60 @@
+from PyQt5.QtWidgets import QLabel, QLineEdit, QComboBox, QCheckBox
+
+from data import scoreboard
+
+
+class SBWidgetPair:
+    def __init__(self, parent, name: str, key: str):
+
+        self.key = key
+        self.label = QLabel(name, parent)
+
+    def on_edited(self):
+        pass
+
+
+class SBTextWidget(SBWidgetPair):
+    def __init__(self, parent, name: str, key: str):
+        super().__init__(parent, name, key)
+
+        self.edit = QLineEdit(parent)
+        self.edit.setText(scoreboard[key])
+        self.edit.textChanged.connect(self.on_edited)
+
+    def on_edited(self):
+        print("updating sb data")
+        scoreboard[self.key] = self.edit.text()
+
+
+class SBDropdownWidget(SBWidgetPair):
+    def __init__(self, parent, name: str, key: str):
+        super().__init__(parent, name, key)
+
+        self.edit = QComboBox(parent)
+        self.edit.currentIndexChanged.connect(self.on_edited)
+
+    def on_edited(self):
+        print("updating sb data")
+        scoreboard[self.key] = self.edit.currentText()
+
+
+class SBWinsWidget(SBWidgetPair):
+    def __init__(self, parent, name: str, key: str):
+        super().__init__(parent, name, key)
+
+        self.btns = [
+            QCheckBox(""),
+            QCheckBox(""),
+            QCheckBox(""),
+        ]
+        for btn in self.btns:
+            btn.toggled.connect(self.on_edited)
+
+    def on_edited(self):
+        print("updating sb data")
+        wins = 0
+        for btn in self.btns:
+            if btn.isChecked():
+                wins += 1
+
+        scoreboard[self.key] = wins
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import sys
+import traceback
+
 from server import start_server, create_server
 from watcher import start_watcher
+from data import scoreboard
+from gui import SBTextWidget, SBDropdownWidget, SBWinsWidget
 import threading
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QKeyEvent
@@ -13,6 +17,9 @@ from PyQt5.QtWidgets import (
     QSizePolicy,
     QSpacerItem,
     QVBoxLayout,
+    QPushButton,
+    QButtonGroup,
+    QCheckBox,
 )
 
 
@@ -34,20 +41,97 @@ class OctogonWidget(QMainWindow):
         smash_tourny_edit = QLineEdit()
         smash_event_edit = QLineEdit()
 
-        grid = QGridLayout()
+        self.sb_p1_name = SBTextWidget(wid, "P1 Name", key="p1.name")
+        self.sb_p2_name = SBTextWidget(wid, "P2 Name", key="p2.name")
+
+        characters = [
+            "Bowser",
+            "Captain Falcon",
+            "Donkey Kong",
+            "Dr Mario",
+            "Falco",
+            "Fox",
+            "Ganondorf",
+            "Ice Climbers",
+            "Jigglypuff",
+            "Kirby",
+            "Link",
+            "Luigi",
+            "Mario",
+            "Marth",
+            "Mewtwo",
+            "Mr Game & Watch",
+            "Ness",
+            "Peach",
+            "Pichu",
+            "Pikachu",
+            "Roy",
+            "Samus",
+            "Yoshi",
+            "Young Link",
+            "Zelda",
+        ]
+
+        self.sb_p1_char = SBDropdownWidget(
+            wid, "Character", key="p1.character"
+        )
+        self.sb_p1_char.edit.addItems(characters)
+
+        self.sb_p2_char = SBDropdownWidget(
+            wid, "Character", key="p2.character"
+        )
+        self.sb_p2_char.edit.addItems(characters)
+
+        # self.sb_p1_wins = QButtonGroup(self)
+        self.sb_p1_wins = SBWinsWidget(self, "Wins", "p1.wins")
+        self.sb_p2_wins = SBWinsWidget(self, "Wins", "p2.wins")
+
+        self.sb_round_title = SBTextWidget(self, "Round Title", "round_title")
+        self.sb_round_games = SBDropdownWidget(self, "Best of", "round_games")
+        self.sb_round_games.edit.addItems(["3", "5"])
+
+        self.sb_update_bt = QPushButton("Update")
+        self.sb_update_bt.clicked.connect(scoreboard.save)
+
+        grid = QGridLayout(wid)
         grid.setSpacing(10)
 
+        # sub-layout for scoreboard options
+        sb_group = QGridLayout(wid)
+        sb_group.addWidget(self.sb_p1_name.label, 0, 0)
+        sb_group.addWidget(self.sb_p1_name.edit, 0, 1, 1, 3)
+        sb_group.addWidget(self.sb_p2_name.label, 0, 4)
+        sb_group.addWidget(self.sb_p2_name.edit, 0, 5, 1, 3)
+        sb_group.addWidget(self.sb_p1_char.label, 1, 0)
+        sb_group.addWidget(self.sb_p1_char.edit, 1, 1, 1, 3)
+        sb_group.addWidget(self.sb_p2_char.label, 1, 4)
+        sb_group.addWidget(self.sb_p2_char.edit, 1, 5, 1, 3)
+
+        sb_group.addWidget(self.sb_p1_wins.label, 2, 0)
+        for i, btn in enumerate(self.sb_p1_wins.btns):
+            sb_group.addWidget(btn, 2, i + 1)
+
+        sb_group.addWidget(self.sb_p2_wins.label, 2, 4)
+        for i, btn in enumerate(self.sb_p2_wins.btns):
+            sb_group.addWidget(btn, 2, i + 5)
+
+        sb_group.addWidget(self.sb_round_title.label, 3, 0)
+        sb_group.addWidget(self.sb_round_title.edit, 3, 1, 1, 3)
+        sb_group.addWidget(self.sb_round_games.label, 3, 4)
+        sb_group.addWidget(self.sb_round_games.edit, 3, 5, 1, 3)
+
+        sb_group.addWidget(self.sb_update_bt, 4, 0, 1, 8)
+
         # sub-layout for smash.gg related options
-        smash_group = QGridLayout(self)
+        smash_group = QGridLayout(wid)
         smash_group.addWidget(smash_title_label, 0, 0)
         smash_group.addWidget(smash_tourny_label, 1, 0)
         smash_group.addWidget(smash_tourny_edit, 1, 1)
         smash_group.addWidget(smash_event_label, 2, 0)
         smash_group.addWidget(smash_event_edit, 2, 1)
 
-        grid.addLayout(smash_group, 0, 0)
-        spacer = QSpacerItem(QSizePolicy.Minimum, QSizePolicy.Expanding)
-        grid.addItem(spacer, 1, 0, 1, -1)
+        grid.addLayout(sb_group, 0, 0)
+        grid.addLayout(smash_group, 1, 0)
 
         wid.setLayout(grid)
 
@@ -94,7 +178,7 @@ def main():
 
         app.exec()
     except Exception as e:
-        print(e)
+        print(traceback.format_exc())
     finally:
         server.shutdown()
         thread.join()
