a13feb6c11201795364464d32f8d0dbd3a30829d
Branden Akana <branden.akana@gmail.com>
2021-02-04 03:26:51 -1000

    implement swap button. resolves #4

--- octogon/main.py
+++ octogon/main.py
@@ -54,9 +54,11 @@ class OctogonWidget(QMainWindow):
         smash_tourny_edit = QLineEdit()
         smash_event_edit = QLineEdit()
 
+        # player names
         self.sb_p1_name = SBTextWidget(wid, "P1 Name", key="p1.name")
         self.sb_p2_name = SBTextWidget(wid, "P2 Name", key="p2.name")
 
+        # characters chosen
         character_names = characters.values()
 
         self.sb_p1_char = SBDropdownWidget(
@@ -69,16 +71,23 @@ class OctogonWidget(QMainWindow):
         )
         self.sb_p2_char.edit.addItems(character_names)
 
+        # number of wins per player
         # self.sb_p1_wins = QButtonGroup(self)
         self.sb_p1_wins = SBWinsWidget(self, "Wins", "p1.wins")
         self.sb_p2_wins = SBWinsWidget(self, "Wins", "p2.wins")
 
+        # round title
         self.sb_round_title = SBTextWidget(self, "Round Title", "round_title")
+
+        # best of 3/5
         self.sb_round_games = SBDropdownWidget(self, "Best of", "round_games")
         self.sb_round_games.edit.addItems(["3", "5"])
 
         self.sb_update_bt = QPushButton("Update")
         self.sb_update_bt.clicked.connect(scoreboard.save)
+
+        self.sb_bt_swap = QPushButton("Swap P1/P2")
+        self.sb_bt_swap.clicked.connect(self.swap)
 
         grid = QGridLayout(wid)
         grid.setSpacing(10)
@@ -108,6 +117,7 @@ class OctogonWidget(QMainWindow):
         sb_group.addWidget(self.sb_round_games.edit, 3, 5, 1, 3)
 
         sb_group.addWidget(self.sb_update_bt, 4, 0, 1, 8)
+        sb_group.addWidget(self.sb_bt_swap, 5, 0, 1, 8)
 
         # sub-layout for smash.gg related options
         smash_group = QGridLayout(wid)
@@ -138,6 +148,27 @@ class OctogonWidget(QMainWindow):
         self.setGeometry(300, 300, 800, 600)
         self.setWindowTitle("Octogon Panel")
         self.show()
+
+    def swap(self):
+        """Swap P1 and P2's information."""
+
+        # swap names
+        p1_name = self.sb_p1_name.edit.text()
+        p2_name = self.sb_p2_name.edit.text()
+        self.sb_p1_name.edit.setText(p2_name)
+        self.sb_p2_name.edit.setText(p1_name)
+
+        # swap characters
+        p1_char = self.sb_p1_char.edit.currentIndex()
+        p2_char = self.sb_p2_char.edit.currentIndex()
+        self.sb_p1_char.edit.setCurrentIndex(p2_char)
+        self.sb_p2_char.edit.setCurrentIndex(p1_char)
+
+        # swap wins
+        p1_wins = [btn.isChecked() for btn in self.sb_p1_wins.btns]
+        p2_wins = [btn.isChecked() for btn in self.sb_p2_wins.btns]
+        [self.sb_p1_wins.btns[i].setChecked(p2_wins[i]) for i in range(len(p1_wins))]
+        [self.sb_p2_wins.btns[i].setChecked(p1_wins[i]) for i in range(len(p2_wins))]
 
     def keyPressEvent(self, e: QKeyEvent):
         if e.key() == Qt.Key_Q:
